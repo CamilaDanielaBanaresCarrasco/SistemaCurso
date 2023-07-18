a5d67c96680b4c9a346f3b4a33e9292214feec81
Camila Banares Carrasco <cbanares@tuxpan.com>
2023-07-17 21:44:51 -0400

    cambios bbdd

--- src/cnn/cnn.js
+++ src/cnn/cnn.js
@@ -1,3 +1,10 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('postgres://postgres:ZSMjohzbIjHVU2vocMXt@containers-us-west-131.railway.app:7341/railway');
+export const sequelize  = new Sequelize('postgres://postgres:ZSMjohzbIjHVU2vocMXt@containers-us-west-131.railway.app:7341/railway');
+
+/* try {
+    await sequelize.authenticate();
+    console.log('La conexion se ah ejecutado con exito.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  } */
