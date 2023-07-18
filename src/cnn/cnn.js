57cca46418d3934978ce15bd5f17891aa412e289
Camila Banares Carrasco <cbanares@tuxpan.com>
2023-07-17 21:03:42 -0400

    first commit

---
+++ src/cnn/cnn.js
@@ -0,0 +1,3 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize('postgres://postgres:ZSMjohzbIjHVU2vocMXt@containers-us-west-131.railway.app:7341/railway');
---
+++ src/models/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { routerEstudiantes } from '../routes/estudiantes.routes.js';
+
+export default class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use('/estudiantes', routerEstudiantes);
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Corriendo en el puerto: ${this.port}`)
+        })
+    }
+
+}
